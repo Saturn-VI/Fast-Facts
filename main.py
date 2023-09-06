0bf05f89bc83f505b6bc1ea92d55c4aebd4fd34c
saturnvi <maxwang420@gmail.com>
2023-09-05 20:58:03 -0500

    started work on weighted table

--- main.py
+++ main.py
@@ -17,3 +17,40 @@ def random_letters():
             
     return return_letters # returns the letters
 
+# Topic info from https://docs.google.com/spreadsheets/d/1gZIRPB9HAy40yGv7lJ9B4XWgIovPfImd1WzFet4s1tA/edit
+
+topics = [
+        "Science",
+        "Scientists",
+        "Female Scientists",
+        "16th-19th Century Scientists",
+        "Astronomers",
+        "Astronauts",
+        "NASA Astronauts",
+        "Biologists",
+        "Chemists",
+        "Physicists",
+        "Nobel Laureates",
+        "Aviators",
+        "Inventors",
+        "Philosophers"
+        ]
+topic_weights = [
+        1, # Science
+        29, # Scientists
+        4, # Female Scientists
+        1, # 16th-19th Century Scientists
+        1, # Astronomers
+        1, # Astronauts
+        1, # NASA Astronauts
+        1, # Biologists
+        4, # Chemists
+        5, # Physicists
+        3, # Nobel Laureates
+        1, # Aviators
+        1, # Inventors
+        1 # Philosophers
+        ]
+
+def random_topics():
+    random.choices(topics, weights=topic_weights, k=5)
