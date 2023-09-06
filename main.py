3d40fc909aa4370a66ac08ad645a4ba7e142743d
saturnvi <maxwang420@gmail.com>
2023-09-05 20:44:54 -0500

    idk it looks better now

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from random import randint
 letters = 'ABCDEFGJIJKLMNOPQRSTUVWXYZ'
 
 def random_letters():
-    return_letters = []
+    return_letters = [] # defining stuff
     adding = True
     lettersAdded = 0
+
     while adding:
         letter_choice = letters[randint(0, 25)]
         if letter_choice not in return_letters: # adds letter if it doesn't already exist
@@ -14,5 +15,5 @@ def random_letters():
         if lettersAdded == 5: # limits letters added to 5
             adding = False
             
-    return return_letters
+    return return_letters # returns the letters
 
